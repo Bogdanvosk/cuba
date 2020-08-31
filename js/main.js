2715f18910386ae07771b83c040e3653928f2da5
unknown <bogdanvoskanov@gmail.com>
2020-09-01 00:41:44 +0300

    Responsive

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    // Slider
+
+    $(".photo__slider").slick({
+        infinite: true,
+        autoplay: true,
+        prevArrow:
+            '<button type="button" class="slick-prev"><img src="img/arrow-left.svg"/></button>',
+        nextArrow:
+            '<button type="button" class="slick-next"><img src="img/arrow-right.svg" /></button>',
+    });
+
+
+    // Burger menu
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+});
+
+
+// let burger = document.querySelector('.menu__btn'),
+//     menu = document.querySelector('.menu__list');
+
+// burger.addEventListener('click', () => {
+//     menu.classList.toggle('show');
+// })
